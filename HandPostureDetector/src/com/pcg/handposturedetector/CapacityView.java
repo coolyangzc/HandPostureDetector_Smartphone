c173d3e6c790299f358eb83ad46d9c3e8cbd8021
coolyangzc <yang-zc13@mails.tsinghua.edu.cn>
2017-01-18 10:24:47 +0800

    Add Pics

--- HandPostureDetector/src/com/pcg/handposturedetector/CapacityView.java
+++ HandPostureDetector/src/com/pcg/handposturedetector/CapacityView.java
@@ -1,4 +1,4 @@
-package com.example.handposturedetector;
+package com.pcg.handposturedetector;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,6 +9,10 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.util.Log;
 import android.view.View;
 
@@ -17,13 +21,14 @@ public class CapacityView extends View implements Runnable {
 	private final int W = 1080;
 	private final int H = 1920;
 	private final int THRESHOLD = 50;
+	private Paint paint = null;
 	private Process ps = null;
 	
 	int[] capaVal = new int[28 * 16];
 	
-	
 	public CapacityView(Context context) {
 		super(context);
+		paint = new Paint();
 		Thread thread = new Thread(this);
 		thread.start();
 	}
@@ -57,11 +62,9 @@ public class CapacityView extends View implements Runnable {
 	
 	@Override
     protected void onDraw(Canvas canvas) {
-		Paint paint = new Paint();
+		super.onDraw(canvas);
+		
         paint.setColor(Color.WHITE);
-        paint.setTextSize(50);
-        paint.setTextAlign(Paint.Align.LEFT);
-        //canvas.drawText(this.getMeasuredHeight() + ", " + this.getMeasuredWidth(), 500, 500, paint);
 
         paint.setStyle(Paint.Style.FILL);
         int squareWidth = W / 16;
@@ -73,6 +76,7 @@ public class CapacityView extends View implements Runnable {
             }
         }
         int colorGradient;
+        float x = 0, y = 0, sum = 0;
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 28; j++) {
             	if (capaVal[i*28+j] <= THRESHOLD)
@@ -81,9 +85,41 @@ public class CapacityView extends View implements Runnable {
             		colorGradient = (int)((double)capaVal[i * 28 + j] / max * 255);
                 paint.setColor(Color.rgb(colorGradient, colorGradient, colorGradient));
                 canvas.drawRect(squareWidth * i, squareHeight * j, squareWidth * (i + 1), squareHeight * (j + 1), paint);
+                x += colorGradient * (i + 0.5f);
+                y += colorGradient * (j + 0.5f);
+                sum += colorGradient;
             }
         }
-        super.onDraw(canvas);
+        x /= sum;
+        y /= sum;
+        float dirX = 0, dirY = 0;
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 28; j++) {
+            	if (capaVal[i*28+j] <= THRESHOLD)
+            		continue;
+            	float dx = x - i;
+            	float dy = y - j;
+            	if (dx*dy >=0) dx = Math.abs(dx);
+            	else dx = - Math.abs(dx);
+            	Log.d("dx", Float.toString(dx));
+            	Log.d("capaVal", Integer.toString(capaVal[i*28+j]));
+            	Log.d("result", Float.toString(dx * capaVal[i*28+j]));
+            	dirX += dx * capaVal[i*28+j];
+            	dirY += dy * capaVal[i*28+j];
+            }
+        }
+        Log.d("dirX", Float.toString(dirX));
+        x = x * squareWidth;
+        y = y * squareHeight;
+        paint.setColor(Color.RED);
+        canvas.drawCircle(x, y, 32, paint);
+        Drawable d;
+        if (dirX >= 0)
+        	d = getResources().getDrawable(R.drawable.v_r);
+        else
+        	d = getResources().getDrawable(R.drawable.v_l);
+        d.setBounds(300, 50, 419 + 300, 268 + 50);
+        d.draw(canvas);
 	}
 
 	@Override
---
+++ HandPostureDetector/src/com/pcg/handposturedetector/MainActivity.java
@@ -0,0 +1,18 @@
+package com.pcg.handposturedetector;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		CapacityView cView = new CapacityView(this);
+		LinearLayout topLayout = new LinearLayout(this);
+		topLayout.addView(cView);
+		setContentView(topLayout);
+		//setContentView(R.layout.activity_main);
+	}
+}
