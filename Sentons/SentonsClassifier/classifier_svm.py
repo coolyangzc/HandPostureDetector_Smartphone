033c81266d129403de1d6796af40245354270074
coolyangzc <yang-zc13@mails.tsinghua.edu.cn>
2017-03-15 14:40:10 +0800

    Fix bug in data_process: random.shuffle

--- Sentons/SentonsClassifier/classifier_svm.py
+++ Sentons/SentonsClassifier/classifier_svm.py
@@ -2,26 +2,32 @@ import sys
 import random
 import numpy as np
 from sklearn import svm
+from sklearn import tree
+from sklearn import neighbors
 from sklearn.model_selection import KFold
+
+print 'File reading...'
+sys.stdout.flush()
 
 filename = '..\Sentons_Data\data.txt'
 fd = file(filename)
 pixels = int(fd.readline()[:-1])
+
 lines = fd.readlines()
+random.shuffle(lines)
+
 X = []
 y = []
 
-print 'File reading...'
-sys.stdout.flush()
 for line in lines:
     data = map(int, line.split(' '))
     X.append(data[:-1])
     y.append(data[-1])
-random.shuffle(X)
-random.shuffle(y)
 
-X = X[:2000]
-y = y[:2000]
+print 'Loaded ' + str(len(X)) + ' data successfully'
+
+X = X[:]
+y = y[:]
 
 X = np.array(X)
 y = np.array(y)
@@ -35,13 +41,14 @@ error = []
 for k, (train_index, test_index) in enumerate(kf.split(X, y)):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
-    clf = svm.SVC(kernel='rbf')
+    clf = svm.SVC()
     clf.fit(X_train, y_train)
     answer_train = clf.predict(X_train)      
     print('training accuracy: '+ str(np.mean(answer_train == y_train)))
     answer_test = clf.predict(X_test)
     print('test accuracy: ' + str(np.mean(answer_test == y_test)))
     error.append(np.mean(answer_test == y_test))    
+
 # Averaging
 print "===========Final Result==========="
 print "Final accuracy: " + str(np.mean(error))
--- Sentons/SentonsClassifier/data_process.py
+++ Sentons/SentonsClassifier/data_process.py
@@ -4,8 +4,8 @@ import os.path
 dir = "..\Sentons_Data"
 
 category = [[] for i in range(2)]
-category[0] = ['V_L', 'V_L_F']
-category[1] = ['V_R', 'V_R_F']
+category[0] = ['V_L_F'] #['V_L', 'V_L_F']
+category[1] = ['V_R_F'] #['V_R', 'V_R_F']
 
 EDGE_MM = 116
 PIXELS = 116
@@ -18,7 +18,7 @@ def process(fd, catg):
     for line in lines[2:]:
         data = line[:-1].split(' ')
         n = int(data[2])
-        if (n == 0):
+        if (n <= 0):
             continue
         p = 3
         edge = [[0 for i in range(PIXELS)] for i in range(2)]
@@ -31,15 +31,19 @@ def process(fd, catg):
             pos1 = int(round(pos1 / EDGE_MM * (PIXELS - 1)))
             for i in range(pos0, pos1 + 1):
                 edge[bar][i] += force
-            for i in range(2):
+            p += 6
+        for i in range(2):
                 for j in range(PIXELS):
                     outfd.write(str(edge[i][j]) + ' ')
-            outfd.write(str(catg) + '\n')
+        outfd.write(str(catg) + '\n')
+            
             
 
 outfd.write(str(PIXELS) + '\n') 
 for parent, dirnames, filenames in os.walk(dir):
     for filename in filenames:
+        if (filename == 'data.txt'):
+            continue
         print 'Reading ' + os.path.join(parent, filename)
         fd = file(os.path.join(parent, filename))
         tag = fd.readline()
